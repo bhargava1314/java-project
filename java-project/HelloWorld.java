2d966069755e879f7513405d6e332ef94a5c0480
Cloud User <ec2-user@ip-172-31-32-53.eu-north-1.compute.internal>
2026-04-19 14:02:40 +0000

    adding java prog

---
+++ java-project/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins + GitHub Integration!");
+        
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+
+        System.out.println("Sum of a and b is: " + sum);
+    }
+}
